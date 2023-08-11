cae484d4e010df5ccd8adc387b4f23431fc63957
Bipin Singh <bipins867@gmail.com>
2023-08-12 00:12:59 +0530

    Add files via upload

--- Nodejs-As-6/app.js
+++ Nodejs-As-6/app.js
@@ -1,43 +1,7 @@
 const http=require('http');
-const fs=require('fs')
+const route=require('./script')
 
-const server=http.createServer((req,res)=>{
-    const url=req.url;
-    const method=req.method;
-
-    if (url=='/'){
-        const data=fs.readFileSync('Hello.txt','utf-8');
-        res.write('<html><body><form action="/message" method="POST" id="form">')
-
-        res.write("<label id='label'>"+data+"</label><br><input type='text' name='msg'/><input type='submit' value='sumbit'/></form>")
-        res.write('</body></html>')
-        return res.end();
-    }
-
-    if(url=='/message' && method=='POST'){
-
-        const body=[];
-
-        req.on('data',chunk=>{
-            body.push(chunk);
-        })
-
-        req.on('end',()=>{
-
-            const parseData=Buffer.concat(body).toString();
-            
-            var data=parseData.split('=')[1];
-            
-            fs.writeFileSync('Hello.txt',data);
-
-        })
-
-
-        res.setHeader('Location','/')
-        res.statusCode='302'
-        res.end();
-    }
-})
-
+const server=http.createServer(route.handler)
+console.log(route.text)
 
 server.listen(3000);
