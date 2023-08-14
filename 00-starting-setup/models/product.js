e36361cd06db31f1c5edfd335c34f8470c518d59
Bipin Singh <bipins867@gmail.com>
2023-08-14 13:06:07 +0530

    Dynamic Routes
    Dynamic Routes with the help of :productId

---
+++ 00-starting-setup/models/product.js
@@ -0,0 +1,49 @@
+const fs = require('fs');
+const path = require('path');
+
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  'data',
+  'products.json'
+);
+
+const getProductsFromFile = cb => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
+    }
+  });
+};
+
+module.exports = class Product {
+  constructor(title, imageUrl, description, price) {
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
+  }
+
+  save() {
+    this.id=new Date().getTime();
+    getProductsFromFile(products => {
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), err => {
+        console.log(err);
+      });
+    });
+  }
+
+  static fetchAll(cb) {
+    getProductsFromFile(cb);
+  }
+
+  static findById(id,cb){
+   getProductsFromFile(product=>{
+    const prodId=product.find(product=>id==product.id)
+    cb(prodId)
+   })
+   
+  }
+};
